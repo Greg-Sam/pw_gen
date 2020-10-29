82f0d744a4757f97274032827254c023282c64ee
Greg Samuelson <grssam@gmail.com>
2020-10-29 09:44:09 -0700

    final version

--- script.js
+++ script.js
@@ -1,71 +1,68 @@
-let length = prompt("How many characters do you want your password to be?")
+const special_array = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '{', '}', ']', '[', '|', '?']
 
-length = parseInt(length)
+const num_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-let special = confirm(`Would you like to use special characters in your password?  
-Select 'OK' for Yes, and 'Cancel' to continue without special characters.`)
+const cap_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-let numbers = confirm(`Would you like to use numbers in your password?
-Select 'OK' for Yes, and 'Cancel' to continue without numbers.`)
+const pw_base = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-let caps = confirm(`Would you lie to use capital letters in your password?
-Select 'OK' for Yes, and 'Cancel' to continue without capital letters.`)
 
-let special_array = ['!', '@', '#', '$']
 
-let num_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+// function generatePassword(length) {}
+function generatePassword() {
 
-let cap_array = ['A', 'B', 'C', 'D']
+  let length = parseInt(prompt("How many characters do you want your password to be?  You must select a length of between 8 and 128 characters."))
 
-let pw_base = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  if ((length < 8) || (length > 128)) {
+    alert("Please enter a number between 8 and 128")
+    window.location.reload()
+  } else {
+    let special = confirm(`Would you like to use special characters in your password?  
+    Select 'OK' for Yes, and 'Cancel' to continue without special characters.`)
 
-let w_special = []
+    let numbers = confirm(`Would you like to use numbers in your password?
+    Select 'OK' for Yes, and 'Cancel' to continue without numbers.`)
 
-if (special === true) {
-  w_special = pw_base.concat(special_array)
-} else {
-  w_special = pw_base
+    let caps = confirm(`Would you lie to use capital letters in your password?
+    Select 'OK' for Yes, and 'Cancel' to continue without capital letters.`)
+
+    let w_special = []
+
+    if (special === true) {
+      w_special = pw_base.concat(special_array)
+    } else {
+      w_special = pw_base
+    }
+
+    let w_numbers = []
+
+    if (numbers === true) {
+      w_numbers = w_special.concat(num_array)
+    } else {
+      w_numbers = w_special
+    }
+
+    let all_char = []
+
+    if (caps === true) {
+      all_char = w_numbers.concat(cap_array)
+    } else {
+      all_char = w_numbers
+    }
+
+    // console.log(all_char)
+
+
+    let myPassword = ''
+    for (let i = 0; i < length; i++) {
+      let pw_char = all_char[Math.floor(Math.random() * all_char.length)]
+      myPassword += pw_char
+    }
+    // console.log(myPassword)
+
+
+    document.getElementById('password').textContent = myPassword
+  }
 }
 
-let w_numbers = []
-
-if (numbers === true) {
-  w_numbers = w_special.concat(num_array)
-} else {
-  w_numbers = w_special
-}
-
-let all_char = []
-
-if (caps === true) {
-  all_char = w_numbers.concat(cap_array)
-} else {
-  all_char = w_numbers
-}
-
-console.log(all_char)
-
-
-
-
-let password = ''
-for (let i = 0; i < length; i++){
-  let pw_char = all_char[Math.floor(Math.random() * all_char.length)]
-  password += pw_char
-}
-console.log(password)
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById('generate').addEventListener("click", generatePassword);
